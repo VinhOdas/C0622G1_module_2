796847e6ffed35b354a1ba90c0cb2c182558b828
Vinh Pham <gamquangvinh@gmail.com>
2022-08-02 21:50:43 +0700

    nop muon t3

--- Module_2/src/ss3_array_method/exercise/AddElementToArray.java
+++ Module_2/src/ss3_array_method/exercise/AddElementToArray.java
@@ -1,10 +1,28 @@
 package ss3_array_method.exercise;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class AddElementToArray {
     public static void main(String[] args) {
-        Scanner sc  = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        int[] arr = {5, 2, 3, 4, 5, 6, 5, 8, 5, 0, 5, 5};
+        System.out.println("Nhập phần tử  mà bạn muốn thêm");
+        int element;
+        element = sc.nextInt();
+        int index;
+        do {
+            System.out.println("Nhập index");
+            index = sc.nextInt();
+            if (index <= -1 || index >= arr.length) {
+                System.out.println("Không được phép thêm vào:");
+            }
+        } while (index <= -1 || index >= arr.length);
+        for (int i = arr.length - 1; index < i; i--) {
+            arr[i] = arr[i -1];
+        }
+        arr[index] = element;
+        System.out.println(Arrays.toString(arr));
 
     }
 }
--- Module_2/src/ss3_array_method/exercise/MergeArray.java
+++ Module_2/src/ss3_array_method/exercise/MergeArray.java
@@ -1,10 +1,22 @@
 package ss3_array_method.exercise;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MergeArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int[] arr = {1,2,5,6,7,8};
+        int[] arr1 = {56,6,34,23,6};
+//        System.out.println("Nhập phần tử  mà bạn muốn tìm");
+//        int length = Integer.parseInt(sc.nextLine());
+        int[] arr2 = new int[arr.length + arr1.length];
+        for (int i = 0; i < arr.length; i++) {
+            arr2[i] = arr[i];
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            arr2[arr.length + i] = arr1[i];
+        }
+        System.out.println(Arrays.toString(arr2));
     }
 }
