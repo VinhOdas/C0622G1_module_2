27ac4b79768d19c5c7c798d805fa4099b3854f08
Vinh Pham <gamquangvinh@gmail.com>
2022-08-20 18:50:09 +0700

    ss15

---
+++ Module_2/src/ss15_exception_handling_debug/excercise/IllegalTriangleException/TriangleException.java
@@ -0,0 +1,7 @@
+package ss15_exception_handling_debug.excercise.IllegalTriangleException;
+
+public class TriangleException extends Exception{
+    public TriangleException(String message) {
+        super(message);
+    }
+}
